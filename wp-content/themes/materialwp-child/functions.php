d7672809f95ca37fa292db74a649f585b08a37d7
Anthony <anthony@MacBook-Pro-de-Anthony.local>
2015-03-09 11:52:40 +0100

    add some test  themes and features

--- wp-content/themes/materialwp-child/functions.php
+++ wp-content/themes/materialwp-child/functions.php
@@ -1,5 +1,8 @@
 <?php
-
+/**
+ * @package materialwpChild
+ * @subpackage Functions
+ **/
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
@@ -9,4 +12,9 @@ function theme_enqueue_styles() {
     );
 }
 
+add_action('wp_enqueue_scripts', 'load_javascript_files');
+function load_javascript_files() {
+	wp_register_script('childjs', get_stylesheet_directory_uri() . '/js/child.js', array('jquery'), true );
+	wp_enqueue_script('childjs');
+}
 ?>
